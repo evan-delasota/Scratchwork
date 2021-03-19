ecec85708f24af23379510067518bc6028106bd7
Evan D <Evan_delasota@yahoo.com>
2021-03-19 22:14:49 +0000

    comments

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 for (let i = 0; i <= 10; ++i) {
   console.log("n = " + i + "\n");
+  //i ^ 3
   console.log("f1: ", Math.pow(i, 3));
+  // i ^ 2 + 10
   console.log("f2: ", 3 * Math.pow(i, 2) + 10);
+  // linebreak
   console.log();
 }
